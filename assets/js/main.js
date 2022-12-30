7626b7333ad37517be621a318fa1dd4331ad6a99
apurba2002 <debnathapurba582@gmail.com>
2022-12-30 13:09:10 +0600

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+function op(){
+    let search_box = document.getElementById('search');
+    search_box.style.opacity=80;
+    
+}
+function contract(){
+    window.location.href="contract.html";
+}
+function home(){
+    window.location.href="index.html";
+}
+function product(){
+    window.location.href="product.html";
+}
+function account (){
+    window,location.href="login.html"
+}
+function facebook(){
+    window.location.href="https://www.facebook.com/apurba.debnathapu/";
+}
+function instragram(){
+    window.location.href="https://www.instagram.com/apurba.debnathapu/";
+    
+}
+
+function youtube(){
+    window.location.href="https://www.youtube.com/channel/UCA9Ldta-rMmrNTBfc_J--wA/featured";
+    
+}
+function product1(){
+   
+    window.location.href="product1.html";
+   
+   
+    
+}
+function product2(){
+    window.location.href="product2.html";
+    
+   
+   
+    
+}
+function product3(){
+    window.location.href="product3.html";
+    
+   
+   
+    
+}
+function product4(){
+    window.location.href="product4.html";
+}    
+function product5(){
+    window.location.href="product5.html";
+}  
+function product6(){
+    window.location.href="product6.html";
+}  
+function signup(){
+    window.location.href="signup.html";
+}
+    
+
---
+++ assets/sql quaries/SQLQuery1.sql
@@ -0,0 +1,7 @@
+select password from login where user_name='apurba'
+update login set password='555923' where user_name='anik'
+insert into login (user_name,password) values ('anik','555920')
+insert into login (user_name,password) values ('adom','555922')
+insert into login (user_name,password) values ('Tahmid','568496')
+insert into login (user_name,password) values ('joya','555920')
+select * from login
